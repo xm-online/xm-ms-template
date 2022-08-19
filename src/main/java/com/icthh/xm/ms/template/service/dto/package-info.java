7fc1ef21b6f78ad9f15d5fbbeb4ec9b828d5f188
Anatoliy Medvedchuk <anatoliy.medvedchuk@jevera.software>
2022-08-19 18:28:32 +0300

    rename packages

--- src/main/java/com/icthh/xm/ms/template/service/dto/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Data Transfer Objects.
- */
-package com.icthh.xm.ms.template.service.dto;
--- src/main/java/com/icthh/xm/ms/template/web/rest/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring MVC REST controllers.
- */
-package com.icthh.xm.ms.template.web.rest;
